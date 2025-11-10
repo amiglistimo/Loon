de9ec97be31fc73bdf58e7f4c0ed9ab955602c04
52hertz <wenzhao1988@gmail.com>
2025-11-10 17:54:22 +0800

    Update AppRaven.js

--- AppRaven.js
+++ AppRaven.js
@@ -19,8 +19,8 @@ body = body.replace(/"premium":false/g, '"premium":true');
 body = body.replace(/"hasInAppPurchases":false/g,'"hasInAppPurchases":true');
 // body = body.replace(/"youOwn":false/g,
 // '"youOwn":true');
-body = body.replace(/"arcade":false/g,
-'"arcade":true');
+// body = body.replace(/"arcade":false/g,
+// '"arcade":true');
 
 // body = body.replace(/"preorder":false/g,
 // '"preorder":true');
