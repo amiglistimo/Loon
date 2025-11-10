785c2c61b5cbb975c3679ef0b6276e45521c53a8
52hertz <wenzhao1988@gmail.com>
2025-11-10 17:52:49 +0800

    Enable premium features in AppRaven.js

--- AppRaven.js
+++ AppRaven.js
@@ -17,8 +17,8 @@ var body = $response.body;
 
 body = body.replace(/"premium":false/g, '"premium":true');
 body = body.replace(/"hasInAppPurchases":false/g,'"hasInAppPurchases":true');
-body = body.replace(/"youOwn":false/g,
-'"youOwn":true');
+// body = body.replace(/"youOwn":false/g,
+// '"youOwn":true');
 body = body.replace(/"arcade":false/g,
 '"arcade":true');
 
