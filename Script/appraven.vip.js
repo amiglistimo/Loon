ec187323f8cda0df8866dbbc8f5c03181cef9544
52hertz <wenzhao1988@gmail.com>
2025-11-11 09:06:45 +0800

    Update appraven.vip.js

--- Script/appraven.vip.js
+++ Script/appraven.vip.js
@@ -1,22 +1,6 @@
-/*
-AppRaven
+var body = $response.body;
 
+body = body.replace(/"premium":false/g, '"premium":true');
+body = body.replace(/"hasInAppPurchases":false/g,'"hasInAppPurchases":true');
 
-
-[Script]
-http-response ^https?:\/\/v2\.appraven\.net\/appraven\/graphql script-path = https://raw.githubusercontent.com/amiglistimo/Loon/refs/heads/main/Script/appraven.vip.js, requires-body = true, tag = AppRaven美化
-
-[MitM]
-hostname = v2.appraven.net
-
-**/
-
-var modifiedHeaders = $request.headers;
-var operationName = modifiedHeaders['X-APOLLO-OPERATION-NAME'];
-
-if (operationName == "GetCurrentUser"||operationName == "GetUserById") {
-  var body = $response.body.replace(/"premium":false/g, '"premium":true');
-  $done({ body: body });
-} else {
-  $done({});
-}
+$done({ body });
