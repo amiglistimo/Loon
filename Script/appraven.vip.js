fb097e6c3a7a108af96e71ac61a55cc540b606eb
52hertz <wenzhao1988@gmail.com>
2025-11-13 11:55:51 +0800

    Delete Script/appraven.vip.js

--- Script/appraven.vip.js
+++
@@ -1,6 +0,0 @@
-var body = $response.body;
-
-body = body.replace(/"premium":false/g, '"premium":true');
-body = body.replace(/"hasInAppPurchases":false/g,'"hasInAppPurchases":true');
-
-$done({ body });
